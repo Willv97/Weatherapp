6b59a326ab71ed0edb8e7fac97ed2eb55c036067
Willv97 <willv1357@gmail.com>
2024-05-08 23:54:58 -0400

    fixed htmle to display location icon, added api inclusion, js variables

--- index.js
+++ index.js
@@ -12,5 +12,32 @@ search.addEventListener('click', () =>{
     if (city === '')
         return;
 
-        fetch('https://')
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+        .then(response => response.json())
+        .then(json => {
+
+            if (json.cod === '404') {
+                container.computedStyleMap.height = '400px';
+                weatherBox.computedStyleMap.display = 'none';
+                weatherDetails.computedStyleMap.display = 'none';
+                error404.computedStyleMap.display = 'block';
+                error404.classList.add('fadein');
+                return;
+            }
+
+            error404.computedStyleMap.display = 'none';
+            error404.classList.remove('fadeIn');
+
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('weather-box .temperature');
+            const description = document.querySelector('weather-details .description');
+            const humidity = document.querySelector('weather-details .humidity');
+            const wind = document.querySelector('weather-details .windspan');
+
+            switch (json.weather[0].main) {
+                case 'Clear':
+                    image.src = 'images/snow.png';
+                    break;
+            }
+        })
 })
